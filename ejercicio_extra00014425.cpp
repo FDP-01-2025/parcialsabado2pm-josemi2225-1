3a1a3a5b6197021fbdb127c35b678a1a4886a35d
josemi2225 <00014425@uca.edu.sv>
2025-06-07 15:16:51 -0600

    update :memo:

---
+++ ejercicio_extra00014425.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+ //declaracion de variables
+ string nombre;
+ int carnet = 7;
+
+cout<< "Porfavor ingrese su nombre\n";
+cin >> nombre;
+
+cout << "Porfavor ingrese su numero de carnet\n";
+cin >> carnet;
+
+ cout<< "Elige una de las de siguentes opciones\n";
+ cout<<"a.Agregar notas\n b.consultar todas las notas\n c.calcular todas las notas \n d.salir\n";
+ 
+
+
+
+    return 0;
+}
