e5a47315bd9186a2314336f34d77e6e5f87f680d
josemi2225 <00014425@uca.edu.sv>
2025-06-07 15:19:52 -0600

    update

---
+++ ejercicio2_for.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //declaracion de variables
+    int n = 0;
+
+
+
+    for (int i = 0; i <= n; i++)
+    {
+        /* code */
+    }
+    
+
+
+
+
+    return 0;
+}
